a447c206d8745a0673c58ff9b3e23a9d79f99cd4
GrPugaguga <pugaguga@mail.ru>
2025-04-21 17:51:31 +0500

    fix  import

--- components/sendpage/constructor/decorationsManager.jsx
+++ components/sendpage/constructor/decorationsManager.jsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState, useEffect } from "react"
-import { getStickers } from "../../../lib/utils/getImages"
-// import { getStickers } from '../../../lib/utils/images';
+// import { getStickers } from "../../../lib/utils/getImages"
+import { getStickers } from '../../../lib/utils/images';
 
 
 /**
