af511956f4dcd3ed17180c67c51d0dc05c10b32c
puga-labs <cyprus.pugamuga@gmail.com>
2025-04-21 13:13:13 +0700

    transition pages nextjs15

--- components/mainpage/Hero.jsx
+++ components/mainpage/Hero.jsx
@@ -1,16 +1,31 @@
-import CurveText from "./CurveText"
+"use client"
+import { motion as m } from "framer-motion"
+import React from "react"
 import MainButtons from "./MainButtons"
 import Image from "next/image"
 import HeroBoxesBottom from "./HeroBoxesBottom"
 
 const Hero = () => {
   return (
-    <div className="w-full min-h-screen bg-purple-200 flex flex-col items-center justify-center relative bg-grid">
+    <m.div
+      className="w-full min-h-screen bg-purple-200 flex flex-col items-center justify-center relative bg-grid"
+      initial="hidden"
+      animate="visible"
+      variants={{
+        hidden: {},
+        visible: { transition: { staggerChildren: 0.4 } },
+      }}
+    >
       <GoosseeHero />
       <MainButtons />
-      <div className="my-12"/>
+      <m.div
+        className="my-12"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.8 }}
+      />
       <HeroBoxesBottom />
-    </div>
+    </m.div>
   )
 }
 
@@ -18,7 +33,7 @@ export default Hero
 
 const GoosseeHero = () => {
   return (
-    <div className=" mb-[-200px] pointer-events-none select-none">
+    <div className="mb-[-200px] pointer-events-none select-none">
       <Image
         src="/hero-goossee2.png"
         alt="Goossee"
@@ -29,7 +44,3 @@ const GoosseeHero = () => {
     </div>
   )
 }
-
-
-
-
